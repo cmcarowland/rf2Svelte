9914ae23138e4e88655c3663f999d0df814eff70
George Rowland <cmcarowland@live.com>
2025-04-29 17:25:12 -0400

    build: Changed path in build.yml back to build and added repo name to base path for adapter config path

--- svelte.config.js
+++ svelte.config.js
@@ -24,7 +24,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH + '/rf2Svelte'
 		}
 	}
 };
